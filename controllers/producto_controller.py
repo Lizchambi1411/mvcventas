a9e3853d47e919b691f59dcd79368d261753b48b
unknown <chambivictoria36@gmail.com>
2025-06-09 20:48:27 -0400

    primer commit

---
+++ controllers/producto_controller.py
@@ -0,0 +1,48 @@
+from flask import request, redirect, url_for, Blueprint
+
+from models.producto_model import Producto
+from views import producto_view
+
+producto_bp = Blueprint('producto', __name__, url_prefix="/productos")
+
+@producto_bp.route("/")
+def index():
+    # Recupera todos los registros de la tabla productos
+    productos = Producto.get_all()
+    return producto_view.list(productos)
+
+@producto_bp.route("/create", methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        stock = request.form['stock']
+        
+        # Usa la clase producto para crear una nueva instancia
+        producto = Producto(descripcion=descripcion, precio=precio, stock=stock)
+        producto.save()
+        return redirect(url_for('producto.index'))
+
+    return producto_view.create()
+
+@producto_bp.route("/edit/<int:id>", methods=['GET', 'POST'])
+def edit(id):
+    producto = Producto.get_by_id(id)
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        precio = request.form['precio']
+        stock = request.form['stock']
+        
+        # Actualizar
+        producto.update(descripcion=descripcion, precio=precio, stock=stock)
+        return redirect(url_for('producto.index'))
+    
+    return producto_view.edit(producto)
+
+@producto_bp.route("/delete/<int:id>")
+def delete(id):
+    producto = producto.get_by_id(id)
+    if producto:  # Verifica si el producto existe
+        producto.delete()
+    return redirect(url_for('producto.index'))
+
